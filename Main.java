7f8600791c53f74534f027efaa90e936d603e677
AndyProger <84966031+AndyProger@users.noreply.github.com>
2021-05-28 12:51:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import  java.lang.String; // Необязательно, так как входит по умолчанию
+
+class FirstJavaApp {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte num0 = 126;
+        int num1 = 2331;
+
+        float num2 = 25.65f;
+        double num3 = 0.12354532d;
+
+        boolean check= true;
+
+        char symbol = 'a';
+        String str = "Hello, World!";
+        System.out.println(str);
+    }
+}
